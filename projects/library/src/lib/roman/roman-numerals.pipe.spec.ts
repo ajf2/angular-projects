157e9381c789b459f7e8dc579562bb339a1f5c0b
ajf2 <ajf2@users.noreply.github.com>
2019-03-23 16:16:01 +0000

    Added upper and lower bound tests.

--- projects/library/src/lib/roman/roman-numerals.pipe.spec.ts
+++ projects/library/src/lib/roman/roman-numerals.pipe.spec.ts
@@ -7,8 +7,16 @@ describe('RomanNumeralsPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
+  it('transforms 1', () => {
+    expect(pipe.transform(1)).toBe('I');
+  });
+
   it('transforms 1997', () => {
     expect(pipe.transform(1997)).toBe('MCMXCVII');
+  });
+
+  it('transforms 3999', () => {
+    expect(pipe.transform(3999)).toBe('MMMCMXCIX');
   });
 
   it('transforms 1997 with basic notation', () => {
