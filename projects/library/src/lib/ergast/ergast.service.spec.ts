740b93716a502f2566858477825240d7e55f38f4
ajf2 <ajf2@users.noreply.github.com>
2019-03-24 22:40:23 +0000

    Fixed service test.

--- projects/library/src/lib/ergast/ergast.service.spec.ts
+++ projects/library/src/lib/ergast/ergast.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErgastService } from './ergast.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ErgastService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: ErgastService;
+  let httpSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        ErgastService,
+        { provide: HttpClient, useValue: spy }
+      ]
+    });
+    service = TestBed.get(ErgastService);
+    httpSpy = TestBed.get(HttpClient);
+  });
 
   it('should be created', () => {
-    const service: ErgastService = TestBed.get(ErgastService);
     expect(service).toBeTruthy();
   });
 });
