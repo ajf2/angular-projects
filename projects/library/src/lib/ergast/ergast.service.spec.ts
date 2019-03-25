0d0980cf9c5458a54dbd7c39f8b18b4045c9c5f3
ajf2 <ajf2@users.noreply.github.com>
2019-03-25 09:29:33 +0000

    Improved service test spec.

--- projects/library/src/lib/ergast/ergast.service.spec.ts
+++ projects/library/src/lib/ergast/ergast.service.spec.ts
@@ -1,25 +1,50 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ErgastService } from './ergast.service';
-import { HttpClient } from '@angular/common/http';
+import { MotorRacingData } from './motor-racing-data';
 
 describe('ErgastService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: ErgastService;
-  let httpSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('HttpClient', ['get']);
     TestBed.configureTestingModule({
-      providers: [
-        ErgastService,
-        { provide: HttpClient, useValue: spy }
-      ]
+      imports: [HttpClientTestingModule]
     });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.get(ErgastService);
-    httpSpy = TestBed.get(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get Observable<MRData> and be called once', () => {
+    const mockData: MotorRacingData = {
+      MRData: {
+        RaceTable: {
+          Races: [],
+          season: 0
+        },
+        limit: 0,
+        offset: 0,
+        series: '',
+        total: 0,
+        url: '',
+        xmlns: ''
+      }
+    };
+    service.currentSeason().subscribe(data => expect(data).toEqual(mockData));
+    // I don't really understand the following yet, but adapted it from the Angular Guide.
+    // https://angular.io/guide/http#testing-http-requests
+    const req = httpTestingController.expectOne('https://ergast.com/api/f1/current.json');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockData);
+    httpTestingController.verify();
   });
 });
